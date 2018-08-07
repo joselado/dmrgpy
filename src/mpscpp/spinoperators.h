01503745b73243e2d86bfc994ec9fdb0f78b4468
Jose Lado <jose@pop-os.localdomain>
2018-08-07 10:26:36 +0200

    New version

--- src/mpscpp/spinoperators.h
+++ src/mpscpp/spinoperators.h
@@ -7,19 +7,19 @@ auto get_spin_operator(auto sites, int i, auto name) {
         		ampo += 0.5,"Cdagdn",i+1,"Cup",i+1;
         		ampo += 0.5,"Cdagup",i+1,"Cdn",i+1;
         	} ;
-		else if (name=="Sy") {
+		if (name=="Sy") {
         		ampo += -0.5,"Cdagdn",i+1,"CupI",i+1;
         		ampo += 0.5,"Cdagup",i+1,"CdnI",i+1;
         	} ;
-		else if (name=="Sz") {
+		if (name=="Sz") {
         		ampo += 0.5,"Cdagup",i+1,"Cup",i+1;
         		ampo += -0.5,"Cdagdn",i+1,"Cdn",i+1;
         	} ;
-		else if (name=="Cdag") {
+		if (name=="Cdag") {
         		ampo += 1.0,"Cdagup",i+1;
         		ampo += 1.0,"Cdagdn",i+1;
         	} ;
-		else if (name=="C") {
+		if (name=="C") {
         		ampo += 1.0,"Cup",i+1;
         		ampo += 1.0,"Cdn",i+1;
         	} ;
