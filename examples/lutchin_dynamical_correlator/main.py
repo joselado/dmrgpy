9bb3a1668eca5f6424e7a0455247cd6c141708de
Jose Lado <jose@pop-os.localdomain>
2018-10-10 22:49:27 +0200

    New version

--- examples/lutchin_dynamical_correlator/main.py
+++ examples/lutchin_dynamical_correlator/main.py
@@ -7,17 +7,13 @@ import matplotlib.pyplot as plt
 import fermionchain
 import geometry
 
-n = 40 
+n = 4
 g = geometry.chain()
-#g = g.supercell(n)
+g = g.supercell(n)
 h = g.get_hamiltonian()
-h.add_rashba(1.0)
-h.shift_fermi(3.0)
-h.add_zeeman(3.0)
-h.add_swave(1.0)
-h.get_bands(operator="sy")
-exit()
-hk = h.get_hk_gen()
+h.add_rashba(0.5)
+h.shift_fermi(1.7)
+h.add_zeeman(0.6)
 
 def fd(i,j):
     if i==j: return 0.1
@@ -26,8 +22,5 @@ def fd(i,j):
 fc = fermionchain.Fermionic_Hamiltonian(len(g.r))
 fc.set_spinful_hoppings(h.intra)
 fc.set_pairing(fd)
-pairs = [(0,i) for i in range(n)]
-out = fc.correlator(pairs)
-import matplotlib.pyplot as plt
-plt.plot(range(n),out,c="blue",marker="o")
-plt.show()
+fc.get_dynamical_correlator(mode="DMRG",i=0,j=0,name="cdc",delta=0.03)
+
