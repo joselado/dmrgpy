b1cf4333beee5b739cbfd757f511d1dc5ee8ed2a
Jose Lado <jose@pop-os.localdomain>
2019-02-15 11:24:07 +0100

    New version

--- examples/spinless_fermion_dynamical_correlator/main.py
+++ examples/spinless_fermion_dynamical_correlator/main.py
@@ -7,20 +7,31 @@ from dmrgpy import fermionchain
 n = 5
 fc = fermionchain.Fermionic_Hamiltonian(n,spinful=False) # create the chain
 m = np.matrix(np.random.random((n,n)) + 1j*np.random.random((n,n)))
-m = m + m.H
+m = m + m.H # Make it Hermitian
+
+
 def ft(i,j):
     return m[i,j]
-    if abs(j-i)==1: return 1.0 
-    return 0.0
+
+
+# Initialize the Hamiltonian
 fc.set_hoppings(ft) # hoppings
 e0 = fc.gs_energy(mode="ED") # energy with exact diagonalization
 e1 = fc.gs_energy(mode="DMRG") # energy with DMRG
-fc.gs_from_file = False
 print("Energy with ED",e0)
 print("Energy with DMRG",e1)
+
+
+### Compute the dyamical correlator ###
+
 name = "densitydensity" # name of the correlator
 x0,y0 = fc.get_dynamical_correlator(i=1,j=1,mode="ED",name=name)
 x1,y1 = fc.get_dynamical_correlator(i=1,j=1,mode="DMRG",name=name)
+
+
+
+### Plot the result ###
+
 import matplotlib.pyplot as plt
 
 plt.plot(x0,y0.real,label="ED")
