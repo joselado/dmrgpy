6b993d3ef81a7670a92c02177bf8a17239c808f6
joselado <jose@pop-os.localdomain>
2019-05-08 10:18:37 +0200

    Update

--- install/install.py
+++ install/install.py
@@ -3,16 +3,23 @@ import os
 # compile Itensor library
 path = os.path.dirname(os.path.realpath(__file__))+"/../" # main path
 os.chdir(path+"/src/dmrgpy/mpscpp/ITensor-master")
-os.system("cp options.save options.mk")
-import cppversion
-if cppversion.correct_version():
-    print("C++ compiler is fine")
-else: # C++ compiler is not the right one
-    import autoinstallubuntu
-    autoinstallubuntu.install() # install dependencies
-    # replace the compiler in options.mk
-    out = open("options.mk").read().replace("CCCOM=g++","CCCOM=g++-6")
-    open("options.mk","w").write(out) # write new file
+import platform
+if platform.system()=="Linux": # Linux system
+  os.system("cp options.save options.mk")
+  print("Detected Linux system")
+  import cppversion
+  if cppversion.correct_version():
+      print("C++ compiler is OK")
+  else: # C++ compiler is not the right one
+      import autoinstallubuntu
+      autoinstallubuntu.install() # install dependencies
+      # replace the compiler in options.mk
+      out = open("options.mk").read().replace("CCCOM=g++","CCCOM=g++-6")
+      open("options.mk","w").write(out) # write new file
+else:
+  print("Detected Mac system")
+  print("You may need to install the GNU C++ compiler")
+  os.system("cp options.mac options.mk")
 # now do the compilation
 os.system("make clean ")
 os.system("make")
