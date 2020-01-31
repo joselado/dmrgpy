7514516c4af9209d04291378e4113eba4cbdb71d
joselado <jose.luis.lado@gmail.com>
2020-01-31 13:41:29 +0200

    Update

--- src/dmrgpy/parallelslurm.py
+++ src/dmrgpy/parallelslurm.py
@@ -8,8 +8,8 @@ def pcall(fin,xs):
     """Run a parallel calculation with slurm"""
     n = len(xs) # number of calculations
     f = lambda x: fin(x)
-    pwd = os.getcwd() # current directory 
     main = "import dill as pickle\nimport os\n"
+    main += "import sys ; sys.path.append(os.environ['DMRGROOT'])\n"
     main += "try: ii = int(os.environ['SLURM_ARRAY_TASK_ID'])\n"
     main += "except: ii = 0\n"
     main += "f = pickle.load(open('function.obj','rb'))\n"
@@ -17,11 +17,12 @@ def pcall(fin,xs):
     main += "folder = 'folder_'+str(ii)\n"
     main += "os.system('mkdir '+folder)\n"
     main += "os.chdir(folder)\n"
-    main += "out = f(v[ii])\n"
-#    main += "try: out = f(v[ii])\n"
-#    main += "except: out = None\n"
+#    main += "out = f(v[ii])\n"
+    main += "try: out = f(v[ii])\n"
+    main += "except: out = None\n"
     main += "print(out)\n"
     main += "pickle.dump(out,open('out.obj','wb'))\n"
+    main += "os.system('touch DONE')\n"
     pfolder = ".parallel"
     os.system("rm -rf "+pfolder) # create directory
     os.system("mkdir "+pfolder) # create directory
@@ -32,10 +33,29 @@ def pcall(fin,xs):
     runsh = "#!/bin/bash\n#SBATCH -n 1\n#SBATCH -t 20:00:00"
     runsh += "#SBATCH --mem-per-cpu=5000\n"
     runsh += "#SBATCH --array=0-"+str(n-1)+"\n"
-    runsh += "srun python main.py\n"
+    runsh += "srun python run.py\n"
     open(pfolder+"/run.sh","w").write(runsh) # parallel file
+    pwd = os.getcwd() # current directory 
     os.chdir(pfolder) # go to the folder
-#    os.system("sbatch run.sh") # run calculation
+    os.system("sbatch run.sh") # run calculation
+    os.chdir(pwd) # back to main
+    import time
+    from os import path
+    time.sleep(0.5) # wait half a second
+    while True:
+        finished = True
+        for i in range(n):
+            if not path.exists(pfolder+"/folder_"+str(i)+"/DONE"):
+                finished = False
+        if finished: break
+    # get all the data
+    ys = []
+    for i in range(n):
+        folder = pfolder+"/folder_"+str(i)+"/"
+        y = pickle.load(open(folder+'out.obj','rb'))
+        ys.append(y)
+    return ys
+
 
 
 
