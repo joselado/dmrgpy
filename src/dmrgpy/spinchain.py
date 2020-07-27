adb44de17882f634833b55321b58b5f4b8a0f81e
joselado <jose.luis.lado@gmail.com>
2020-07-27 16:04:58 +0300

    Update

--- src/dmrgpy/spinchain.py
+++ src/dmrgpy/spinchain.py
@@ -59,11 +59,12 @@ class Spin_Chain(Many_Body_Chain):
         Sx = self.Sx
         Sy = self.Sy
         Sz = self.Sz
-        for i in range(ntries):
-            for j in range(ntries):
-                op = Sx[i]*Sy[j] - Sx[i]*Sy[j]
-                if i==j: op = op - 1j*Sz[i]
-                if not self.is_zero_operator(op,**kwargs): raise
+        for ii in range(ntries):
+            i = np.random.randint(self.ns)
+            j = np.random.randint(self.ns)
+            op = Sx[i]*Sy[j] - Sy[j]*Sx[i]
+            if i==j: op = op - 1j*Sz[i]
+            if not self.is_zero_operator(op,**kwargs): raise
     def set_exchange(self,fun):
       """Set the exchange coupling between sites"""
       h = 0
