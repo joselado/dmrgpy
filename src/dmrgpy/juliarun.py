9e1ccfb763810f462ddc2bf780c95bdb3a9d3aeb
Jose Lado <joselado@pop-os.localdomain>
2020-08-05 10:01:39 +0300

    Update

--- src/dmrgpy/juliarun.py
+++ src/dmrgpy/juliarun.py
@@ -20,27 +20,36 @@ if not hasjulia: # no julia present, add manually to the path
 
 precompile = False
 
+# check the system image
+sysimage = os.environ["HOME"]+"/.julia/sysimages/sys_itensors.so"
+if not os.path.isfile(sysimage):
+    print("No ITensors system image found, this may take some time")
+    sysimage = None
+
+
 from julia.api import Julia
-jlsession = Julia(compiled_modules=False) # start the Julia session
+jlsession = Julia(compiled_modules=False,
+        sysimage=sysimage) # start the Julia session
 jlsession.eval("using Suppressor") # suppress output
 
 
 def run(self):
-    if precompile:
-        mpsjl = dmrgpath+"/mpsjulia/mpsjulia.jl"
-        txt = "using Fezzik\n" # Fezzik package
-        txt += "Fezzik.auto_trace()\n"
-        txt += "include(\""+mpsjl+"\")\n"
-        txt += "Fezzik.brute_build_julia()\n"
-        def fun():
-            f = open("run_and_compile.jl","w")
-            f.write(txt)
-            f.close()
-        self.execute(fun) # write the compilation script
-   #     exit()
-        print("Precompiling Julia, this may take a while")
-        self.execute(lambda: os.system("julia run_and_compile.jl > status.jl"))
-        print("FINISHED precompiling Julia")
+    if precompile: # precompile the ITensor code
+        jlsession.eval("using ITensors; ITensors.compile()")
+#        mpsjl = dmrgpath+"/mpsjulia/mpsjulia.jl"
+#        txt = "using Fezzik\n" # Fezzik package
+#        txt += "Fezzik.auto_trace()\n"
+#        txt += "include(\""+mpsjl+"\")\n"
+#        txt += "Fezzik.brute_build_julia()\n"
+#        def fun():
+#            f = open("run_and_compile.jl","w")
+#            f.write(txt)
+#            f.close()
+#        self.execute(fun) # write the compilation script
+#   #     exit()
+#        print("Precompiling Julia, this may take a while")
+#        self.execute(lambda: os.system("julia run_and_compile.jl > status.jl"))
+#        print("FINISHED precompiling Julia")
     else:
         import contextlib
         c = "@suppress_out include(\""+dmrgpath+"/mpsjulia/mpsjulia.jl\");"
