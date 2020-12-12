8dd1a1882b07367a752e3348129af9986f789982
joselado <jose.luis.lado@gmail.com>
2020-12-12 10:11:29 +0200

    Update

--- examples/SYK_chain/main.py
+++ examples/SYK_chain/main.py
@@ -4,23 +4,28 @@ import os ; import sys ; sys.path.append(os.getcwd()+'/../../src')
 import numpy as np
 import matplotlib.pyplot as plt
 from dmrgpy import fermionchain
-n = 5
-fc = fermionchain.Fermionic_Chain(n) # create the chain
+n = 3
+L = 5
+fc = fermionchain.Fermionic_Chain(n*L) # create the chain
 h = 0
 #for i in range(n):
 #  for j in range(i):
 #    h = h + np.random.random()*fc.C[i]*fc.Cdag[j]
-C = fc.A
-Cdag = fc.Adag
-for i in range(n):
-  for j in range(n):
-    for k in range(n):
-      for l in range(n):
-        Ni = C[i]*Cdag[j]
-        Nj = C[k]*Cdag[l]
-        h = h + np.random.random()*Ni*Nj
+C = fc.C
+Cdag = fc.Cdag
+for ll in range(L):
+  for i in range(n):
+    for j in range(n):
+      for k in range(n):
+        for l in range(n):
+          Ni = C[i+ll*n]*Cdag[j+ll*n]
+          Nj = C[k+ll*n]*Cdag[l+ll*n]
+          h = h + np.random.random()*Ni*Nj
+          if ll<(L-1):
+            Ni = C[i+ll*n]*Cdag[j+ll*n]
+            Nj = C[k+(ll+1)*n]*Cdag[l+(ll+1)*n]
+            h = h + np.random.random()*Ni*Nj
 h = h + h.get_dagger()
-#exit()
 
 ms = [3,10,20,1000]
 #ms = [10]
@@ -46,7 +51,11 @@ e1 = fc.gs_energy(mode="DMRG") # energy with DMRG
 t1 = time.time()
 print("Time in GS = ",t1-t0)
 #fc.mpomaxm = 500
-fc.get_dynamical_correlator(name=(fc.C[0],fc.C[1]))
+(x,y) = fc.get_dynamical_correlator(name=(fc.Cdag[0],fc.C[0]))
+import matplotlib.pyplot as plt
+plt.plot(x,y)
+plt.show()
+exit()
 t2 = time.time()
 print("Time in correlator = ",t2-t1)
 
