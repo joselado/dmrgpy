851403df3b43d088318d37e84d00d93d1487987e
joselado <jose.luis.lado@gmail.com>
2020-12-29 10:47:44 +0200

    Update

---
+++ examples/arnoldi_ground_state/main.py
@@ -0,0 +1,19 @@
+# Add the root path of the dmrgpy library
+import os ; import sys ; sys.path.append(os.getcwd()+'/../../src')
+
+import numpy as np
+from dmrgpy import spinchain
+n = 10
+spins = ["S=1/2" for i in range(n)] # spin 1/2 heisenberg chain
+sc = spinchain.Spin_Chain(spins) # create the spin chain
+h = 0
+for i in range(n-1):
+    h = h +np.random.random()*sc.Sx[i]*sc.Sx[i+1]
+    h = h +np.random.random()*sc.Sy[i]*sc.Sy[i+1]
+    h = h +np.random.random()*sc.Sz[i]*sc.Sz[i+1]
+
+sc.set_hamiltonian(h)
+#print(sc.get_excited(n=40,mode="ED")) ; exit()
+from dmrgpy import mpsalgebra
+wf = mpsalgebra.mpsarnoldi(sc,h,mode="GS",n=4)
+print(sc.gs_energy())
--- src/dmrgpy/algebra/arnolditk.py
+++ src/dmrgpy/algebra/arnolditk.py
@@ -4,16 +4,33 @@ import scipy.linalg as lg
 # routines to perform iterative diagonalization
 
 def mpsarnoldi(self,H,wf=None,e=0.0,delta=1e-1,
-        n=6,maxde=1e-4,mode="SI",maxit=30,**kwargs):
+        mode="SI",**kwargs):
     """Compute an eigenvector using the Arnoldi algorithm"""
+    if mode=="SI": # target a specific energy
+        M = H - (e+delta*1j) # shift
+        Op = lambda x: self.applyinverse(M,x) # operator to apply
+        def fe(es): # function return the right WF
+            es = np.abs(es-e) # minimum energy
+            return np.where(es==np.min(es))[0][0] # return the index
+    elif mode=="GS": # target the ground state
+        shift = self.ns/2. # number of sites
+        M = H-shift # start with the Hamiltonian
+        Op = lambda x: M*x # operator to apply
+        def fe(es): # function return the right WF
+            return np.where(es==np.min(es))[0][0] # return the index
+    else: raise
+    return mpsarnoldi_iteration(self,Op,H,wf,fe,**kwargs)
+
+def mpsarnoldi_iteration(self,Op,H,wf,fe,maxit=30,maxde=1e-4,
+        verbose=1,n=6):
+    """Single iteration of the restarted arnoldi algorithm"""
     wfs = []
     if wf is None:
         wf = self.random_mps() # random initial guess
         wf = wf.normalize()
     else: wfs.append(wf)
-    M = H - (e+delta*1j) # shift
     for i in range(n-len(wfs)): # loop over Krylov vectors
-        wf = self.applyinverse(M,wf,**kwargs) # apply the inverse
+        wf = Op(wf) # apply operator
         wf = gram_smith_single(wf,wfs) # orthogonalize
         if wf is None: break # stop the loop
         wfs.append(wf.copy()) # store
@@ -23,20 +40,20 @@ def mpsarnoldi(self,H,wf=None,e=0.0,delta=1e-1,
       for j in range(nw):
           mh[i,j] = wfs[j].dot(H*wfs[i]) # compute representation
     (es,vs) = lg.eigh(mh)
-    es = es-e # difference
-    v0 = [y for (x,y) in sorted(zip(np.abs(es),vs.T))][0]
+    inde = fe(es) # get the index
+    v0 = vs.T[inde] # get the wavefunction
     wf = 0
     for i in range(nw): wf = wf + np.conjugate(v0[i])*wfs[i] # add
     wf = wf.normalize()
     ef = wf.dot(H*wf).real
     ef2 = wf.dot(H*(H*wf)).real
     error = ef2-ef**2
-    print("Error",error,"Energy",ef)
+    if verbose>0: print("Error",error,"Energy",ef)
     if error<maxde: return wf
     if maxit<0: return wf
     if nw==1: return wf
-    else: return mpsarnoldi(self,H,wf=wf,e=e,mode=mode,
-            delta=delta,n=n,maxit=maxit-1,**kwargs)
+    else: return mpsarnoldi_iteration(self,Op,H,wf,fe,maxit=maxit-1,
+            maxde=maxde,verbose=verbose,n=n)
 
 
 
