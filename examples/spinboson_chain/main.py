61403f8f7b8da31c18563dbabf13f9dded0c2b0d
joselado <jose.luis.lado@gmail.com>
2021-01-16 12:49:55 +0200

    Update

--- examples/spinboson_chain/main.py
+++ examples/spinboson_chain/main.py
@@ -13,9 +13,9 @@ t = 1.0 # hopping
 U = 1.0 # Hubbard
 mu = 0.0 # chemical potential
 # add the bosonic part
-for i in range(ns,nb-1):# loop over bosons (hopping)
+for i in range(ns,ns+nb-1):# loop over bosons (hopping)
     h = h +t*bc.Adag[i]*bc.A[i+1] # hopping
-for i in range(ns,nb):# loop over bosons (interaction and chemical potential)
+for i in range(ns,ns+nb):# loop over bosons (interaction and chemical potential)
     h = h + U*(bc.N[i]-1.0)*(bc.N[i]-1.0) # onsite interaction
     h = h + mu*bc.N[i] # chemical potential
 # add the spin spin part
@@ -29,7 +29,7 @@ h = h + h.get_dagger()
 bc.set_hamiltonian(h)
 e = bc.gs_energy() # compute the ground state energy
 print("Energy",e)
-for i in range(ns,nb):
+for i in range(ns,ns+nb):
     print()
     print("Site #",i)
     print("Average occupation",np.round(bc.vev(bc.N[i]).real,2))
