e4aeef820e05898e609b7a03c663b8e2ca75f573
joselado <jose.luis.lado@gmail.com>
2021-08-16 11:11:02 +0300

    Update

---
+++ examples/evolve_wavefunction/main.py
@@ -0,0 +1,35 @@
+#------------------------------------------------------------------
+# Add the root path of the dmrgpy library
+import os ; import sys ; sys.path.append(os.getcwd()+'/../../src')
+import numpy as np
+from dmrgpy import spinchain
+
+# Heisenberg Hamiltonian S12
+n = 4 # number of sites in your chain
+spins = ["S=1/2" for i in range(n)] # create the sites
+sc = spinchain.Spin_Chain(spins) # create the chain
+h = 0
+for i in range(n-1):
+    h = h + sc.Sx[i]*sc.Sx[i+1]
+    h = h + sc.Sy[i]*sc.Sy[i+1]
+    h = h + sc.Sz[i]*sc.Sz[i+1]
+h = h + h.get_dagger()
+sc.set_hamiltonian(h) # set Hamiltonian
+
+# set default initial state as ground state
+wf = sc.get_gs() # get the ground state
+wf = sc.Sx[0]*wf 
+wf = wf.normalize() 
+wf0 = wf.copy()
+
+# change second value for different times
+
+def get_wf(h, ts, wf_initial=wf0):
+    ts = np.linspace(0.0, 1, 1) 
+    from dmrgpy.timeevolution import imaginary_exponential # function to perform t-evol
+    wfs = imaginary_exponential(h,wf_initial,ts=ts) # this computes e^{iht}*wf for each t 
+    wf_final = wfs[1]
+    return wf_final 
+
+wf_final = get_wf(h, ts, wf_initial=wf0)
+
---
+++ examples/time_evolution_wavefunction/main.py
@@ -0,0 +1,46 @@
+# Add the root path of the dmrgpy library
+import os ; import sys ; sys.path.append(os.getcwd()+'/../../src')
+
+import numpy as np
+from dmrgpy import spinchain
+n = 2 # number of sites in the chain
+spins = ["S=1/2" for i in range(n)] # S=1/2
+sc = spinchain.Spin_Chain(spins) # create the chain
+
+# first Hamiltonian, to create an initial state
+h0 = 0
+for i in range(n): h0 = h0+(-1)**i*sc.Sz[i]
+sc.set_hamiltonian(h0) # set AF hamiltonian
+wf0 = sc.get_gs() # compute ground state
+
+# second Hamiltonian, this will make the time evolution
+h = 0
+for i in range(n-1):
+    h = h+sc.Sx[i]*sc.Sx[i+1] + sc.Sy[i]*sc.Sy[i+1] + sc.Sz[i]*sc.Sz[i+1] 
+h = (h +h.get_dagger())
+
+
+from dmrgpy.timeevolution import imaginary_exponential as evolve
+
+def evolve_and_entropy(t):
+    """Evolve for some time and compute the entropy of the resulting state"""
+    wf = evolve(h,wf0,ts=[t])[0]
+    return wf.get_site_entropy(0)
+    return wf.get_bond_entropy(0,1)
+
+# compute entropies for different times
+ts = np.linspace(0.,4.,40)
+ss = []
+for t in ts:
+    s = evolve_and_entropy(t)
+    print(t,s)
+    ss.append(s)
+
+
+import matplotlib.pyplot as plt
+
+
+plt.plot(ts,ss,marker="o")
+plt.xlabel("Time")
+plt.ylabel("Entropy")
+plt.show()
