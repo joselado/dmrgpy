3c5a05ff0c223499d8679aecdd6e7d2644f8941f
joselado <jose.luis.lado@gmail.com>
2021-09-05 18:59:20 +0300

    Update

--- examples/density_matrix/main.py
+++ examples/density_matrix/main.py
@@ -5,23 +5,34 @@ import os ; import sys ; sys.path.append(os.getcwd()+'/../../src')
 import numpy as np
 import matplotlib.pyplot as plt
 from dmrgpy import fermionchain
-n = 10
+n = 6
 fc = fermionchain.Fermionic_Chain(n) # create the chain
 
-h = 0
-U = 0.3
 
-for i in range(n-1):
-    h = h + fc.Cdag[i]*fc.C[i+1]
-    h = h + U*(fc.N[i]-0.5)*(fc.N[i+1]-0.5)
+def get(U):
+    h = 0
+    for i in range(n-1):
+        h = h + fc.Cdag[i]*fc.C[i+1]
+        h = h + U*(fc.N[i]-0.5)*(fc.N[i+1]-0.5)
+    
+    h = h + h.get_dagger()
+    
+    fc.set_hamiltonian(h) # Hamiltonian
+    wf = fc.get_gs(mode="ED") # compute ground state
+    h = 0
+    
+    dm = wf.get_dm(inds=range(n))
+    import scipy.linalg as lg
+    return lg.eigvalsh(dm)
 
 
-h = h + h.get_dagger()
+import matplotlib.pyplot as plt
 
-fc.set_hamiltonian(h) # Hamiltonian
-wf = fc.get_gs(mode="ED") # compute ground state
+Us = np.linspace(0.,20.0,20)
 
-dm = wf.get_dm(inds=range(n))
-#print(dm)
-import scipy.linalg as lg
-print(np.round(lg.eigvalsh(dm),3))
+for U in Us:
+    y = get(U)
+    plt.scatter(0*y+U,y)
+
+plt.show()
+
