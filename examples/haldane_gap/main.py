74fe9d26923154105704c02a6aea723c240cb313
joselado <jose.luis.lado@gmail.com>
2021-11-25 16:34:17 +0200

    Update

--- examples/haldane_gap/main.py
+++ examples/haldane_gap/main.py
@@ -2,17 +2,19 @@
 import os ; import sys ; sys.path.append(os.getcwd()+'/../../src')
 
 from dmrgpy import spinchain
-n = 40 # take n sites
-spins = [3 for i in range(n)] # spin 1 Heisenberg chain
+n = 18 # take n sites
+spins = ["S=1" for i in range(n)] # spin 1 Heisenberg chain
 sc = spinchain.Spin_Chain(spins) # create the spin chain 
-def fj(i,j): # function for the coupling (open boundary)
-  if abs(j-i)==1: return 1.0 # first neighbor to the right
-  else: return 0.0 # anything else
 sc.maxm = 20
 sc.nsweeps = 30
-sc.set_exchange(fj) # Add coupling between the spins
+h = 0
+for i in range(n-1):
+    h = h + sc.Sx[i]*sc.Sx[i+1]
+    h = h + sc.Sy[i]*sc.Sy[i+1]
+    h = h + sc.Sz[i]*sc.Sz[i+1]
+sc.set_hamiltonian(h)
 # compute n excited states
-es = sc.get_excited(n=5) # compute excited states with DMRG
+es = sc.get_excited(n=6) # compute excited states with DMRG
 # for the Haldane chain with open boundary conditions,
 # in the limit of infnite length we expect a four-fold degeneracy
 # (that correspond to the dangling S=1/2 on the edges)
@@ -20,6 +22,7 @@ es = sc.get_excited(n=5) # compute excited states with DMRG
 # between 1st and the 5th state
 gap = es[4] - es[0]
 print("Haldane gap =",gap)
+print("Energies",es-es[0])
 
 
 
