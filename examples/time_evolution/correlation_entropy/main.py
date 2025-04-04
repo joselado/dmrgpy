7e04c845772f21f5ad7bb6e1f6dc6773622a9241
joselado <jose.lado@aalto.fi>
2025-04-04 14:37:00 +0300

    Update

--- examples/time_evolution/correlation_entropy/main.py
+++ examples/time_evolution/correlation_entropy/main.py
@@ -24,27 +24,48 @@ def get_entropy(V=0):
     wf0 = fc.get_gs(mode=mode) # compute ground state
     wf = wf0.copy() # copy wavefunction
     wf = fc.Cdag[1]*fc.C[0]*wf0 # create an electron-hole pair
+    wf = wf.normalize() # normalize wavefunction
     ents = [] # entanglement entropies
+    occs0 = []
+    occs1 = []
     ts = [] # times
     for i in range(nt): # loop over time steps
         ents.append(wf.get_correlation_entropy()) # compute correlation entropy
+        occs0.append(wf.dot(fc.N[0]*wf)) # occupation in site 0
+        occs1.append(wf.dot(fc.N[1]*wf)) # occupation in site 1
         ts.append(i*dt) # store time
         wf = fc.exponential(1j*dt*h,wf) # apply e^{iHdt}|WF>
-    return ts,ents
+    return ts,ents,occs0,occs1
 
 
 import matplotlib.pyplot as plt
 
 Vs = [0.,1.,2.] # interactions
 
-for V in Vs:
-    (ts,ents) = get_entropy(V=V)
-    plt.plot(ts,ents,marker="o",label="V = "+str(V))
+fig = plt.figure(figsize=(12,4))
 
-plt.xlabel("Time")
-plt.ylabel("Correlation entropy")
-plt.ylim([-0.1,1.])
+for V in Vs:
+    (ts,ents,occs0,occs1) = get_entropy(V=V)
+    plt.subplot(1,3,1)
+    plt.plot(ts,ents,marker="o",label="V = "+str(V))
+    plt.subplot(1,3,2)
+    plt.plot(ts,occs0,marker="o",label="V = "+str(V))
+    plt.subplot(1,3,3)
+    plt.plot(ts,occs1,marker="o",label="V = "+str(V))
+plt.subplot(1,3,1)
+plt.xlabel("Time"); plt.ylabel("Correlation entropy") ; plt.ylim([-0.1,1.])
 plt.legend()
+
+plt.subplot(1,3,2)
+plt.xlabel("Time"); plt.ylabel("Occupation in 0") ; plt.ylim([-0.1,1.1])
+plt.legend()
+
+plt.subplot(1,3,3)
+plt.xlabel("Time"); plt.ylabel("Occupation in 1") ; plt.ylim([-0.1,1.1])
+plt.legend()
+
+
+plt.tight_layout()
 plt.show()
 
 
